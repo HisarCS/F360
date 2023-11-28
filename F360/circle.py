b65cf1b98c7f2470e0803aa74dc3f840d197c665
Emre Dayangaç <120194760+EmreDay1@users.noreply.github.com>
2023-11-28 11:37:11 +0300

    Update circle.py
    deleted the application code making just the classes remain

--- F360/circle.py
+++ F360/circle.py
@@ -11,30 +11,3 @@ class CircleCreator:
     def create_circle(self, sketch):
         circles = sketch.sketchCurves.sketchCircles
         return circles.addByCenterRadius(self.center, self.radius)
-
-def run(context):
-    ui = None
-    try:
-        app = adsk.core.Application.get()
-        ui = app.userInterface
-
-
-        design = app.activeProduct
-
-
-        rootComp = design.rootComponent
-
-  
-        sketches = rootComp.sketches
-        xyPlane = rootComp.xYConstructionPlane
-        sketch = sketches.add(xyPlane)
-
-
-        circle_creator = CircleCreator(adsk.core.Point3D.create(0, 0, 0), 2)
-
-
-        circle1 = circle_creator.create_circle(sketch)
-
-    except:
-        if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
