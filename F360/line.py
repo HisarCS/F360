83e515d23d31fcab966221d0c8afda8cf1e27865
Emre Dayangaç <120194760+EmreDay1@users.noreply.github.com>
2023-11-28 16:12:52 +0300

    Update line.py

--- F360/line.py
+++ F360/line.py
@@ -1,23 +1,18 @@
 import adsk.core, adsk.fusion, adsk.cam, traceback
 from time import sleep
 
-def run(context):
-    ui = None
-    try:
-        class LineDrawer:
-            def __init__(self, start_point, end_point, construction_plane):
+class LineDrawer:
+            def __init__(self, start_point, end_point, plane):
                 self.start_point = start_point
                 self.end_point = end_point
-                self.construction_plane = construction_plane
-
+                self.plane = plane
                 app = adsk.core.Application.get()
                 self.design = app.activeProduct
-
                 self.root_comp = self.design.rootComponent
 
             def create_line(self):
                 sketches = self.root_comp.sketches
-                sketch = sketches.add(self.construction_plane)
+                sketch = sketches.add(self.plane)
 
                 lines = sketch.sketchCurves.sketchLines
 
