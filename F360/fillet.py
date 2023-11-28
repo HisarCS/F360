363bbf388d906ea61d72b6afa6c55d95ab5fb487
Emre Dayangaç <120194760+EmreDay1@users.noreply.github.com>
2023-11-28 11:38:12 +0300

    Update fillet.py

--- F360/fillet.py
+++ F360/fillet.py
@@ -48,37 +48,3 @@ class FilletManager:
         chord_length_edge_set.continuity = adsk.fusion.SurfaceContinuityTypes.TangentSurfaceContinuityType
 
         self.fillets.add(input3)
-
-
-def run(context):
-    try:
-        
-        fillet_manager = FilletManager()
-
-        # Create constant-radius fillet
-        edges1 = fillet_manager.rootComp.bRepBodies.item(0).faces.item(0).edges
-        edge_collection1 = adsk.core.ObjectCollection.create()
-        for edge in edges1:
-            edge_collection1.add(edge)
-        radius1 = adsk.core.ValueInput.createByReal(0.3)
-        fillet_manager.create_constant_radius_fillet(edge_collection1, radius1)
-
-        # Create variable-radius fillet
-        edge2 = fillet_manager.rootComp.bRepBodies.item(0).faces.item(0).edges.item(0)
-        start_radius2 = adsk.core.ValueInput.createByReal(1.0)
-        end_radius2 = adsk.core.ValueInput.createByReal(5.0)
-        positions2 = [adsk.core.ValueInput.createByReal(0.3), adsk.core.ValueInput.createByReal(0.6)]
-        radii2 = [adsk.core.ValueInput.createByReal(2.0), adsk.core.ValueInput.createByReal(3.0)]
-        fillet_manager.create_variable_radius_fillet(edge2, start_radius2, end_radius2, radii2, positions2)
-
-        # Create chord-length fillet
-        edges3 = fillet_manager.rootComp.bRepBodies.item(0).faces.item(0).edges
-        edge_collection3 = adsk.core.ObjectCollection.create()
-        for edge in edges3:
-            edge_collection3.add(edge)
-        chord_length3 = adsk.core.ValueInput.createByReal(1.0)
-        fillet_manager.create_chord_length_fillet(edge_collection3, chord_length3)
-
-
-    except Exception as e:
-        fillet_manager.ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
