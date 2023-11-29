e435ea23658fd4b33aa0f092c75160516da41224
Emre Dayangaç <120194760+EmreDay1@users.noreply.github.com>
2023-11-29 11:34:26 +0300

    Update rectangle.py
    plane input

--- F360/rectangle.py
+++ F360/rectangle.py
@@ -3,24 +3,19 @@ import adsk.fusion
 import traceback
 
 class RectangleDrawer:
-    def __init__(self, corner1, corner2):
+    def __init__(self, corner1, corner2, plane):
         self.corner1 = corner1
         self.corner2 = corner2
+        self.plane = plane
 
         app = adsk.core.Application.get()
         self.design = app.activeProduct
-
-        
         self.root_comp = self.design.rootComponent
 
     def draw_rectangle(self):
-       
         sketches = self.root_comp.sketches
-        xy_plane = self.root_comp.xYConstructionPlane
-        sketch = sketches.add(xy_plane)
+        sketch = sketches.add(self.plane)
 
-  
         sketch.sketchCurves.sketchLines.addTwoPointRectangle(self.corner1, self.corner2)
-
 
  
