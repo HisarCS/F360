6eed59c7c42ae7da0a7d1b5ef7425adea869f049
Emre Dayangaç <120194760+EmreDay1@users.noreply.github.com>
2024-03-28 14:07:26 +0300

    geri mi döndük???

--- F360/Loft.py
+++ F360/Loft.py
@@ -1,45 +1,24 @@
-import adsk.core
-import adsk.fusion
-import traceback
+import adsk.core, adsk.fusion, adsk.cam, traceback
 
-def run(context):
-    ui = None
-    try:
-        app = adsk.core.Application.get()
-        ui = app.userInterface
+class Lofter:
+    def __init__(self, app):
+        self.app = app
+        self.ui = app.userInterface
+        self.design = app.activeProduct
+        self.rootComp = self.design.rootComponent
+        self.profiles = []
+        self.planes = []
 
+    def add_profile(self, plane, sketch_function):
+        sketches = self.rootComp.sketches
+        sketch = sketches.add(plane)
+        sketch_function(sketch)
+        self.profiles.append(sketch.profiles[0])
 
-        design = app.activeProduct
-
-
-        rootComp = design.rootComponent
-        sketches = rootComp.sketches
-
-  
-        sketch1 = sketches.add(rootComp.xYConstructionPlane)
-        circles1 = sketch1.sketchCurves.sketchCircles
-        circle1 = circles1.addByCenterRadius(adsk.core.Point3D.create(0, 0, 0), 10)
-
-        sketch2 = sketches.add(rootComp.xYConstructionPlane)
-        circles2 = sketch2.sketchCurves.sketchCircles
-        circle2 = circles2.addByCenterRadius(adsk.core.Point3D.create(0, 0, 20), 5)
-
-        
-        loftFeatures = rootComp.features.loftFeatures
-        loftInput = loftFeatures.createInput(adsk.fusion.FeatureOperations.NewBodyFeatureOperation)
+    def create_loft(self):
+        loftFeats = self.rootComp.features.loftFeatures
+        loftInput = loftFeats.createInput(adsk.fusion.FeatureOperations.NewBodyFeatureOperation)
         loftSections = loftInput.loftSections
-        loftSections.add(circle1)
-        loftSections.add(circle2)
-
-    
-        loftInput.isSolid = True
-
-        
-        loftFeatures.add(loftInput)
-
-    except:
-        if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
-
-# Run the script
-run(None)
+        for profile in self.profiles:
+            loftSections.add(profile)
+        loftFeats.add(loftInput)
